c93485e6585261b2813a889d10673a36e4f19830
Alex J <94213401+alexjiang903@users.noreply.github.com>
2024-12-15 11:57:16 -0700

    code clean up

--- main.py
+++ main.py
@@ -13,20 +13,7 @@ sheet = client.open("LeetCode Problems Tracker")
 worksheet = sheet.get_worksheet(0)
 data = worksheet.get_all_records()
 
-all_q_df = pd.DataFrame(data) # Data frame of all questions completed so far
-
-question_bank = []
-question_col = all_q_df["Problem Name"] # Column containing names of all all past LC questions done 
-
-for i in range(len(question_col)):
-    question_bank.append(question_col[i])
-
-chosen_question = ""
-random_idx = random.randint(0, len(question_bank) - 1)
-chosen_question = question_bank[random_idx]
-
-print(f"The question to review will be : {chosen_question}")
-
+chosen_question = ut.getRandomQuestion(pd.DataFrame(data))
 
 # Make an API call to get more info about question:
 title_slug = ut.convertToSlug(chosen_question)
@@ -35,26 +22,9 @@ api_URL = f"https://alfa-leetcode-api.onrender.com/select?titleSlug={title_slug}
 
 response = requests.get(api_URL)
 
-
 if response.status_code == 200:
     problem_data = response.json() # raw API response data
-    print(f"Link to question: {problem_data['link']}")
-    print(f"Difficulty: {problem_data['difficulty']}")
-
-    q_tags = problem_data['topicTags'] # Array of tags from API response
-    relevant_topics = []
-
-    for item in q_tags:
-        relevant_topics.append(item["name"])
-
-    print(f"Question Topics: {relevant_topics}")
+    ut.getProblemData(problem_data)
 
 else:
     print(f"Error Code: {response.status_code}")
-
-
-
-
-
-
-
--- utils.py
+++ utils.py
@@ -1,8 +1,35 @@
-# All utility/helper functions go here. 
+# All utility/helper functions are located here
+import pandas as pd
+import random
 
 def convertToSlug(title):
     return title.lower().replace(' ', '-')
 
+def getRandomQuestion(all_q_df):
+    question_col = all_q_df["Problem Name"] # Column containing names of all all past LC questions done 
+    question_bank = []
+    for i in range(len(question_col)):
+        question_bank.append(question_col[i])
 
+    chosen_question = ""
+    random_idx = random.randint(0, len(question_bank) - 1)
+    chosen_question = question_bank[random_idx]
+
+    print(f"The question to review will be : {chosen_question}")
+    return chosen_question
+
+
+
+def getProblemData(problem_data):
+    print(f"Link to question: {problem_data['link']}")
+    print(f"Difficulty: {problem_data['difficulty']}")
+
+    q_tags = problem_data['topicTags'] # Array of tags from API response
+    relevant_topics = []
+
+    for item in q_tags:
+        relevant_topics.append(item["name"])
+
+    print(f"Question Topics: {relevant_topics}")
 
 
