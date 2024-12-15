08a9faaf0e86b83d119c51f304ac2ab662446a64
Alex J <94213401+alexjiang903@users.noreply.github.com>
2024-12-14 22:16:57 -0700

    fetch random question from sheet

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-# API Key: AIzaSyCO8XFcVSocbx4ZEkkX6UiqqClitvq1Lpo
-
-# API endpoint: https://sheets.googleapis.com/v4/spreadsheets/{SHEET_ID}/values/{SHEET_NAME}!A1:Z?alt=json&key={API_KEY}
-
-SHEET_ID = "15DI1H9-EqIZ2KEXeIguB98tmE3vQECJEVUZOnU6AQRE"
-SHEET_NAME = "LeetCode%20Problems%20Tracker"
-API_KEY = "AIzaSyCO8XFcVSocbx4ZEkkX6UiqqClitvq1Lpo"
-
-print(f"https://sheets.googleapis.com/v4/spreadsheets/{SHEET_ID}/values/{SHEET_NAME}!A:Z?alt=json&key={API_KEY}")
-
 import gspread
 import pandas as pd
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
+import random
 
 
+scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+creds = Credentials.from_service_account_file('client_secret.json', scopes=scope)
+client = gspread.authorize(creds)
+
+sheet = client.open("LeetCode Problems Tracker")
+worksheet = sheet.get_worksheet(0)
+data = worksheet.get_all_records()
+
+
+all_q_df = pd.DataFrame(data) # Data frame of all questions completed so far
+
+question_bank = []
+
+all_qs = all_q_df["Problem Name"]# Names of all past LC questions done 
+
+for i in range(len(all_qs)):
+    question_bank.append(all_qs[i])
+
+# print(question_bank)
+chosen_question = ""
+
+random_idx = random.randint(0, len(question_bank) - 1)
+chosen_question = question_bank[random_idx]
+
+print(f"The question to review will be : {chosen_question}")
+
+
+
+#print(data)
 
 
 
