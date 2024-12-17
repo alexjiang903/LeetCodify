322fc883e2eba47338aca5ae9db174888987c04e
Alex J <94213401+alexjiang903@users.noreply.github.com>
2024-12-17 16:29:29 -0700

    added ability to filter question based on selected difficulty

--- main.py
+++ main.py
@@ -13,7 +13,13 @@ sheet = client.open("LeetCode Problems Tracker")
 worksheet = sheet.get_worksheet(0)
 data = worksheet.get_all_records()
 
-chosen_question = ut.getRandomQuestion(pd.DataFrame(data))
+user_select = input("Easy, medium, or hard question to review?: ").lower().capitalize()
+
+if (user_select not in ["Easy", "Medium", "Hard"]):
+    print("Invalid difficulty level. Please try again.")
+    exit()
+
+chosen_question = ut.getRandomQuestion(pd.DataFrame(data), user_select)
 
 # Make an API call to get more info about question:
 title_slug = ut.convertToSlug(chosen_question)
@@ -25,6 +31,8 @@ response = requests.get(api_URL)
 if response.status_code == 200:
     problem_data = response.json() # raw API response data
     ut.getProblemData(problem_data)
+
+
 
 else:
     print(f"Error Code: {response.status_code}")
--- utils.py
+++ utils.py
@@ -5,31 +5,31 @@ import random
 def convertToSlug(title):
     return title.lower().replace(' ', '-')
 
-def getRandomQuestion(all_q_df):
-    question_col = all_q_df["Problem Name"] # Column containing names of all all past LC questions done 
-    question_bank = []
-    for i in range(len(question_col)):
-        question_bank.append(question_col[i])
-
-    chosen_question = ""
-    random_idx = random.randint(0, len(question_bank) - 1)
-    chosen_question = question_bank[random_idx]
-
+def getRandomQuestion(all_q_df, diff):
+    df = all_q_df[['Problem Name', 'Difficulty']]
+    
+    # Filter data frame by difficulty selected
+    df_filtered = df.query('Difficulty == @diff')    
+  
+    chosen_question = df_filtered["Problem Name"].sample(1).iloc[0] #Randomly sample one question from data frame
     print(f"The question to review will be : {chosen_question}")
     return chosen_question
 
 
-
 def getProblemData(problem_data):
-    print(f"Link to question: {problem_data['link']}")
-    print(f"Difficulty: {problem_data['difficulty']}")
+    if 'link' not in problem_data:
+        print("No link found!")
+    else:
+        print(f"Link to question: {problem_data['link']}")
+        print(f"Difficulty: {problem_data['difficulty']}")
 
-    q_tags = problem_data['topicTags'] # Array of tags from API response
-    relevant_topics = []
+        q_tags = problem_data['topicTags'] # Array of tags from API response
+        relevant_topics = []
 
-    for item in q_tags:
-        relevant_topics.append(item["name"])
+        for item in q_tags:
+            relevant_topics.append(item["name"])
 
-    print(f"Question Topics: {relevant_topics}")
+        print(f"Question Topics: {relevant_topics}")
 
+        
 
