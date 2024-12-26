d05571158197a41b18644ebe3178b5a9a6c51ed8
Alex J <94213401+alexjiang903@users.noreply.github.com>
2024-12-26 12:58:02 -0700

    enhanced UI and error handling, language improvements

--- main.py
+++ main.py
@@ -12,19 +12,28 @@ client = gspread.authorize(creds)
 sheet = client.open("LeetCode Problems Tracker")
 worksheet = sheet.get_worksheet(0)
 data = worksheet.get_all_records()
+pandasDataframe = pd.DataFrame(data) # All worksheet data in a pandas dataframe
+all_languages = pandasDataframe["Language"].unique()
+all_difficulties = pandasDataframe["Difficulty"].unique()
+
+
+print("\nWelcome to Leetcodify!")
+print("What question would you like to review today?")
+print("\nThe current languages and difficulties available are:\n")
+# Print out all available languages and difficulties tracked in the worksheet
+print(f"Languages: {', '.join(all_languages)}")
+print(f"Difficulties: {', '.join(all_difficulties)}\n")
 
 user_select_diff = input("Easy, medium, or hard question to review?: ").lower().capitalize()
 
 if (user_select_diff not in ["Easy", "Medium", "Hard"]):
-    print("Invalid difficulty level entered. Please try again.")
+    print("Invalid difficulty level entered. Please try again.\n")
     exit()
 
 user_select_lang = input("What language do you want to review?: ").lower().capitalize()
-
-chosen_question = ut.getRandomQuestion(pd.DataFrame(data), user_select_diff, user_select_lang)
+chosen_question = ut.getRandomQuestion(pandasDataframe, user_select_diff, user_select_lang)
 
 # Make an API call to get more info about question:
-
 title_slug = chosen_question.lower().replace(' ', '-')
 api_URL = f"https://alfa-leetcode-api.onrender.com/select?titleSlug={title_slug}"
 
@@ -32,18 +41,21 @@ response = requests.get(api_URL)
 
 if response.status_code == 200:
     problem_data = response.json() # raw API response data
+    if not problem_data:
+        print("Error occurred in fetching problem data. Please try again.\n")
+        exit()
     ut.getProblemData(problem_data, worksheet)
 
 elif response.status_code == 429:
-    print("LeetCode API is busy. Please try again later.")
+    print("\nLeetCode API is busy. Please try again later.")
     try:
-        print("Response body:", response.text)
+        print(f"Response body:{response.text}\n")
     except Exception as e:
-        print("Error getting details:", e)
+        print(f"Error getting details: {e}\n")
         print(response)
 
     exit()
 
 else:
-    print(f"Error code encountered: {response.status_code}")
+    print(f"Error code encountered: {response.status_code}\n")
     exit()
--- utils.py
+++ utils.py
@@ -16,50 +16,46 @@ def getRandomQuestion(all_q_df, diff, lang):
     all_languages = all_q_df["Language"].unique()
     all_languages = [x.lower() for x in all_languages]
     
-    print("All languages available: ", all_languages)
-    print("All difficulties available: ", df['Difficulty'].unique())
-
     is_valid_lang = lang.lower() in all_languages
     is_valid_diff = diff in df['Difficulty'].unique()   
-
-    print("Valid Language?: ", is_valid_lang)
-    print("Valid Diff?: ", is_valid_diff)
     
     if not (is_valid_lang and is_valid_diff):
         lang = lang if (lang != "Javascript" and lang != "Typescript") else ("JS" if lang == "Javascript" else "TS")
-        print(f"You have not done any questions with language {lang} and difficulty {diff} so far!")
+        print(f"\nYou have not done any questions with language {lang} and difficulty {diff} so far!\n")
         exit()
         
     # Filter data frame by difficulty selected
-    print("lang: ", lang)
+    
     if (lang == "Javascript" or lang == "Typescript"):
-        lang.replace("s", "S") #edge case for JS/TS where the first letter isn't the only one capitalized
+        lang = lang.replace("s", "S") #edge case for JS/TS where the first letter isn't the only one capitalized
+
     
     df_filtered = df.query('Difficulty == @diff and Language == @lang')    
     
     if (df_filtered.empty):
-        lang = lang if (lang != "Javascript" and lang != "Typescript") else ("JS" if lang == "Javascript" else "TS")
-        print(f"No matching questions found with {lang} and difficulty {diff}.")
+        lang = lang if (lang != "JavaScript" and lang != "TypeScript") else ("JS" if lang == "JavaScript" else "TS")
+        print(f"\nNo matching questions found with {lang} and difficulty {diff}.\n")
         exit()
 
     chosen_question = df_filtered["Problem Name"].sample(1).iloc[0] #Randomly sample one question from data frame
-    print(f"The question to review will be : {chosen_question}")
+    print(f"\nThe question to review will be : {chosen_question}")
     return chosen_question
 
 
 def getProblemData(problem_data, sheet_ref):
     if 'link' not in problem_data:
-        print(f"Link error occurred. Problem data recieved: {problem_data}")
+        print(f"Link to problem could not be found.")
+        print(f"Problem data recieved: {problem_data}\n")
         exit()
     
     else:
-        print(f"Link to question: {problem_data['link']}")
+        print(f"\nLink to question: {problem_data['link']}")
         print(f"Difficulty: {problem_data['difficulty']}")
 
         q_tags = problem_data['topicTags'] # Array of tags from API response
         relevant_topics = [item.get("name", "") for item in q_tags if "name" in item] 
         
-        print(f"Question Topics: {relevant_topics}")
+        print(f"Question Topics: {', '.join(relevant_topics)}")
 
         trackLastReview(problem_data["questionTitle"], sheet_ref) # sheet_ref is the worksheet containing all tracked questions
 
@@ -69,12 +65,15 @@ def trackLastReview(q_name, sheet_ref):
     sheet_data = sheet_ref.get_all_records()
     df = pd.DataFrame(sheet_data)
 
-    if 'Most Recent Review' not in df.columns:
+    '''
+        if 'Most Recent Review' not in df.columns:
         print("this shouldn't get triggered anymore... (Most Recent Review already exists in the sheet)")
         df['Most Recent Review'] = None #initialize column if last review date missing
-        sheet_ref.update_acell("K1", "Most Recent Review")
-        sheet_ref.format('K1', formats["header_format"])
-    
+        sheet_ref.update_acell("J1", "Most Recent Review")
+        sheet_ref.format('J1', formats["header_format"])
+     
+    '''   
+  
     # Find the corresponding question:
     current_date = datetime.now().strftime("%Y-%m-%d")
 
@@ -88,8 +87,8 @@ def trackLastReview(q_name, sheet_ref):
         # Write the data back to the sheet (insert/add to "Most Recent Review" column) 
         sheet_ref.update_acell(f"J{row_index[0] + 2}", current_date)
         sheet_ref.format(f"J{row_index[0] + 2}", formats["text_entry_format"])
-        print(f"Successfully updated last review date {current_date} for {q_name}")
+        print(f"Successfully updated last review date {current_date} for {q_name}\n")
     else:
-        print("Error, row index not found.")
+        print("Error, row index not found.\n")
         exit()
         
